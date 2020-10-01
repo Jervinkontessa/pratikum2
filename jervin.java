95ed6a04e99b79d3c38c8520500bb6b1560f882d
Jervin descrates kontessa <71800787+Jervinkontessa@users.noreply.github.com>
2020-10-01 10:38:06 +0800

    Add files via upload

---
+++ jervin.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+class jervin{
+	public static void main (String[] args){
+		
+       System.out.println("Hai saya jervin ");
+       System.out.println("Umur saya adalah 20");
+       System.out.println("saya sedang belajar java");
+       		
+		
+	}
+}
